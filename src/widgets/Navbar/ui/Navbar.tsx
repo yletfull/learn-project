3f75a0c814eb2ac96ed1b44aaf4008153706b62f
Andrey Korlyakov <akorlyakov@ubic.tech>
2022-10-11 17:52:44 +0300

    i18n feature all

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,12 +2,15 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss'
 import React from "react";
 import AppLink, {AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {useTranslation} from "react-i18next";
 
 interface NavbarProps {
   className?: string,
 }
 
 const Navbar = ({className}: NavbarProps) => {
+  const {t} = useTranslation();
+
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
       <div className={cls.links}>
@@ -16,13 +19,13 @@ const Navbar = ({className}: NavbarProps) => {
           theme={AppLinkTheme.SECONDARY}
           className={cls.mainLink}
         >
-          Главная
+          {t('Главная')}
         </AppLink>
         <AppLink
           to={'/about'}
           theme={AppLinkTheme.SECONDARY}
         >
-          О сайте
+          {t('О сайте')}
         </AppLink>
       </div>
     </div>
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import cls from './Sidebar.module.scss'
 import React, {FC, useState} from "react";
 import ThemeSwitcher from "widgets/ThemeSwitcher";
 import LangSwitcher from "widgets/LangSwitcher";
+import {useTranslation} from "react-i18next";
 
 interface SidebarProps {
   className?: string,
@@ -18,13 +19,15 @@ const Sidebar:FC<SidebarProps> = (props) => {
 
   const handleToggle = () => setCollapsed(prev => !prev)
 
+  const {t} = useTranslation();
+
   return (
     <div
       className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
       {...otherProps}
     >
       <button onClick={handleToggle} className={cls.switchButton}>
-        {collapsed ? 'Свернуть' : 'Развернуть'}
+        {collapsed ? t('Свернуть') : t('Развернуть')}
       </button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
