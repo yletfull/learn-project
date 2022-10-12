0246fec6830c6eb5990809a9a9e2b489c7dbf2f4
Andrey Korlyakov <akorlyakov@ubic.tech>
2022-10-12 10:26:42 +0300

    add babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,50 +4,47 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
-    const svgLoader = {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-    }
-
-    const fileLoader = {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-              {
-                  loader: 'file-loader',
-              },
-          ],
+  const svgLoader = {
+    test: /\.svg$/, use: ['@svgr/webpack'],
   }
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: "css-loader",
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]'
-                    },
-                }
-            },
-            "sass-loader",
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i, use: [{
+      loader: 'file-loader',
+    },],
+  }
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i, use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, {
+      loader: "css-loader", options: {
+        modules: {
+          auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+          localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+        },
+      }
+    }, "sass-loader",],
+  }
+
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/, exclude: /node_modules/, use: {
+      loader: "babel-loader", options: {
+        presets: ['@babel/preset-env'],
+        "plugins": [
+          "i18next-extract",
         ],
+      }
     }
+  }
 
-    // Если не используем тайпскрипт - нужен babel-loader
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+  const typescriptLoader = {
+    test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/,
+  }
 
-    return [
-        svgLoader,
-        fileLoader,
-        typescriptLoader,
-        cssLoader,
-    ]
+  return [
+    svgLoader,
+    fileLoader,
+    babelLoader,
+    typescriptLoader,
+    cssLoader,
+  ];
 }
